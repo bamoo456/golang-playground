eb7e466f725861859f9c5ebc7644b5a4c494622d
George Chen <george.chen@etouches.com>
2017-08-20 22:33:14 +0800

    Added basic reflect practice main.go

---
+++ practices/reflect/basic/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var i int = 3
+	t := reflect.TypeOf(i)
+	v := reflect.ValueOf(i)
+	fmt.Printf("The type of integer [i] : %s\n", t)
+	fmt.Printf("The value of integer [i] : %v\n", v)
+
+	t1 := v.Type() // t1 is the same as t
+	fmt.Println(t1)
+
+	iface := v.Interface() // iface now is an "interface{}" type
+
+	// because the iface is an "interface{}", so we can
+	// do the type asertion
+	v1 := iface.(int)
+
+	fmt.Printf("v1 type: %T, value: %v\n", v1, v1)
+
+	// do the normal integer reflection
+	v1Type := getReflectType(v)
+	fmt.Println(v1Type)
+
+	// do the map reflection
+	mType := map[string]int{}
+	mType["1st"] = 1
+	mType["2nd"] = 2
+	mReflectValue := reflect.ValueOf(mType)
+	mReflectValueType := getReflectType(mReflectValue)
+	fmt.Println(mReflectValueType)
+
+}
+
+func getReflectType(v reflect.Value) string {
+	// use v.Kind() to do the switch
+	switch v.Kind() {
+	case reflect.Invalid:
+		return "invalid"
+	case reflect.Int, reflect.Int32, reflect.Int64:
+		return "int number"
+	case reflect.Map:
+		fmt.Println("[getReflectType] map keys", v.MapKeys())
+		return "got the map"
+	default:
+		return "not implemented"
+	}
+}
